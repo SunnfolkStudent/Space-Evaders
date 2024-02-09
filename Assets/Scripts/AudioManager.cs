a1465dbc085d8bc3344460c98c2acc4eb89715f4
Behell <gmarcusgilbert@gmail.com>
2024-02-09 14:44:22 +0100

    audiomanager

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance;
+    
+    public Sound[] musicSound, sfxSounds;
+    public AudioSource musicSource, sfxSource;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+
+    private void Start()
+    {
+        PlayMusic("Theme");
+    }
+
+    public void PlayMusic(string name)
+    {
+        Sound s = Array.Find(musicSound, x => x.name == name);
+
+        if (s == null)
+        {
+            Debug.Log("sound not found");
+        }
+        else
+        {
+            musicSource.clip = s.clip;
+            musicSource.Play();
+        }
+    }
+
+    public void PlaySFX(string name)
+    {
+        Sound s = Array.Find(sfxSounds, x => x.name == name);
+
+        if (s == null)
+        {
+            Debug.Log("sound not found");
+        }
+        else
+        {
+            sfxSource.PlayOneShot(s.clip);
+        }
+    }
+    
+    
+}
